291d7f8d8648e76b19ea10a747a86488f6bd555b
wan.yuyang <wan.yuyang@zte.com.cn>
2020-12-18 05:30:55 +0800

    使用模板显示内容《1》

--- mysite/article/models.py
+++ mysite/article/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
+    DoesNotExist = None
+    objects = None #先声明
     title = models.CharField(max_length=30)
     content = models.TextField()
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from article.views import article_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('article/<int:article_id>', article_detail,name = "article_detail"),
 ]
